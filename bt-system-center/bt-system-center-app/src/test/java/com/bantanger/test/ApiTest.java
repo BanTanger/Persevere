22912ec5fd250e3f18bb61ad1e8c6771e1f2fdbd
bantanger <sharksharkchan@outlook.com>
2025-04-08 11:19:07 +0800

    docs: 链路分析知识学习

--- bt-system-center/bt-system-center-app/src/test/java/com/bantanger/test/ApiTest.java
+++ bt-system-center/bt-system-center-app/src/test/java/com/bantanger/test/ApiTest.java
@@ -7,6 +7,7 @@ import com.bantanger.domain.trade.order.enums.OrderState;
 import com.bantanger.domain.trade.order.enums.OrderType;
 import com.bantanger.domain.trade.order.repository.OrderBaseRepository;
 import com.bantanger.domain.user.AccountType;
+import com.google.common.collect.ImmutableList;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
+import java.util.*;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -40,6 +42,35 @@ public class ApiTest {
         orderBase.setValidStatus(ValidStatus.VALID);
         orderBase.setInvoiceFlag(ValidStatus.INVALID);
         orderBaseRepository.save(orderBase);
+    }
+
+    @Test
+    public void test2() {
+//        ImmutableList<List<String>> lists = ImmutableList.of(
+//                List.of("A", "B", "C", "D"),
+//                List.of("A", "D", "E", "F"),
+//                List.of("A", "B", "C", "F")
+//        );
+        ImmutableList<List<String>> lists = ImmutableList.of(
+                List.of("A", "B", "C", "F", "H"),
+                List.of("A", "D", "C", "F"),
+                List.of("A", "B", "E", "G")
+        );
+        Set<String> allDealIds = new LinkedHashSet<>();
+        // 获取多个集合中长度最大的集合长度，即 max{list1.length, list2.length, list3.length}
+//        int maxSize = lists.stream().mapToInt(List::size).max().orElse(0);
+//        for (int i = 0; i < maxSize; i++) {
+//            // 按照 “电影、演出、玩乐” 的顺序组织到 allDealIds 集合，如 A1,B1,C1,A2,B2,C2 ...
+//            for (List<String> subCategoryDeals : lists) {
+//                int j = 0;
+//                while (j < subCategoryDeals.size() && !allDealIds.add(subCategoryDeals.get(j ++))) {}
+//            }
+//        }
+        int maxSize = lists.stream().mapToInt(List::size).max().orElse(0);
+        for (int i = 0; i < maxSize; i ++) {
+            lists.forEach(list -> list.stream().filter(allDealIds::add).findFirst());
+        }
+        allDealIds.forEach(System.out::print);
     }
 
 }
